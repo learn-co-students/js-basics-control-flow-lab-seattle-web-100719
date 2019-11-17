21ee24ec16c42b8936199894781544b811b97131
learn-co-kat <twothistles@gmail.com>
2019-11-16 16:44:30 -0800

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let reply
+    if (number <= 400) {
+      reply = 'This one is on me!';
+    }
+      if (number > 2000) {
+        reply = 'I will gladly take your thirty bucks.';
+      }
+        if (number > 2500){
+          reply = 'No can do.';
+        }
+  return reply
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let reply
+    if (city === 'NYC'){
+      reply = 'Ok, sounds good.';
+    }
+      else {
+        reply = 'No go.';
+      }
+  return reply
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(comment){
+  let reply
+    if (comment === 'generous'){
+      reply = 'Thank you so much.';
+    }
+      else if (comment === 'not as generous'){
+        reply = 'Thank you.';
+      }
+        else {
+          reply = 'Bye.';
+        }
+  return reply
 }
